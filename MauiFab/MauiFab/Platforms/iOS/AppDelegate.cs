27a1a55cf82ae55574102b44218909595c10f601
cemreis0 <cemmreis@gmail.com>
2024-12-07 10:40:58 +0300

    absolute layout maui fab

---
+++ MauiFab/MauiFab/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiFab
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
